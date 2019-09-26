b566992866863510df6e2628fe72bcd7ce40db8f
GunnarGranlund <55276362+GunnarGranlund@users.noreply.github.com>
2019-09-26 11:55:06 +0200

    Add files via upload

--- P2.py
+++ P2.py
@@ -2,32 +2,59 @@ import numpy as np
 
 #===============================================================================
 # The optimization problem class. The consctructor takes an objective function f,
-# a grid x, and the opportunity to calculate the gradient as inputs
+# a grid x, the dimension n, and the opportunity to calculate the gradient as inputs
 #===============================================================================
 # Vi borde skriva om detta så att function går från Rn -> R, enligt slidsen. Men har 
 # mest testat så att jag får ut rätt saker nu i R
 
 class optimizationProblem(object):
-    def __init__(self, f, x, gradient = True):
+    def __init__(self, f, n):
         self.f = f
-        self.x = x
-        self.h = 0.000001
+        self.h = 0.00001
+        self.n = n
+          
+    def gradient(self, x):
+        gradient = np.zeros(self.n)
+        for i in range(self.n):
+            x_new = x.copy()
+            x_new[i] = x[i] + self.h
+            gradient[i] = (f(x_new) - f(x))/self.h
+        return gradient
 
-        if gradient:
-            self.gradient = np.zeros(len(x))
-            for i in range(len(x)):
-                self.gradient[i] = (f(x[i] + self.h) - f(x[i]))/self.h
-
+    def hessian(self, x):
+        hessian = np.zeros((self.n, self.n))
+        for i in range(self.n):
+            for j in range(self.n):
+                x_new1 = x.copy()
+                x_new1[i] = x[i] + self.h
+                x_new2 = x.copy()
+                x_new2[i] = x[i] - self.h
+                hessian[i,j] = 
+        return (f(x + self.h) - 2*f(x) + f(x - self.h))/self.h**2
+        
     
-class QuasiNewton(optimizationProblem):
+class BaseMethod(optimizationProblem):
     #Skicka in initial guess som input kanske
     #Behöver räkna ut vår Hessianmatris
+    def __init__(self, initial):
+        self.initial = initial
+    
+
 
 #Testsaker
 
 def f(x):
-    return x**2 + 2*x + 1
-    
-x = np.linspace(0.0, 10.0, 10)
-opt = optimizationProblem(f, x)
+    return 100*(x[1] - x[0]**2)**2 + (1 - x[0])**2
+ 
+x1 = np.linspace(0.0, 9.0, 10)
+x2 = np.linspace(0.0, 9.0, 10)
+
+x = np.array(([x1, x2]))
+xx1 = 1.0
+xx2 = 1.0
+
+xx = np.append(xx1, xx2)
+#xx = np.append(x1.T, x2.T)
+opt = optimizationProblem(f, 2)
+print(opt.gradient(xx))
 
