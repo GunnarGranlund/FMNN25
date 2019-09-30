d8bc4679193d7856d30679f69fbbfb566e4c547a
GunnarGranlund <55276362+GunnarGranlund@users.noreply.github.com>
2019-09-30 07:29:01 +0200

    Add files via upload

--- P2.py
+++ P2.py
@@ -1,5 +1,6 @@
 import numpy as np
 import scipy.linalg as sp
+import matplotlib.pyplot as plt
 # ===============================================================================
 # The optimization problem class. The consctructor takes an objective function f,
 # a grid x, the dimension n, and the opportunity to calculate the gradient as inputs
@@ -21,9 +22,9 @@ class OptimizationProblem(object):
         self.f = f
         self.h = 0.00001
         self.n = n
+        self.Ginv = np.eye(n)
 
     def __call__(self, number):
-        print("Hello: ", number)
         return
 
     def g(self, x):  # Calculates the gradient of the objective function f, for given
@@ -44,31 +45,49 @@ class OptimizationProblem(object):
                 h2[j] = self.h
                 G[i, j] = (f(x + h1 + h2) - f(x + h1) - f(x + h2) + f(x)) / self.h ** 2
                 if i != j:  # Symmetrizing step
-                    print(G[i, j])
+                    #print(G[i, j])
                     if G[i, j] == 0:
                         G[i, j] = 0.00001
                     G[i, j] = G[j, i]
         return G
+
+    def invG(self, x):
+        G = self.G(x)
+        L = np.linalg.cholesky(G)
+        Linv = np.linalg.inv(L)
+        self.Ginv = np.dot(Linv, Linv.T)
+    
+    def posDefCheck(self, x):
+        try:
+            np.linalg.cholesky(self.G(x))
+        except np.linalg.LinAlgError:
+            print("Ej positivt definit matris, testa annan initial guess")
 
 
 class BaseMethods:
     def __init__(self, opt):
         self.opt = opt
 
-    def __call__(self, initial_guess, type):
+    def __call__(self, initial_guess, alpha, type):
         if type == 'newton':
-            return self.newton(initial_guess)
+            return self.newton(initial_guess, alpha)
         else:
             print("Can't find given type.")
 
-    def newton(self, x_prev):
+    def newton(self, x_prev, alpha):
         while 1:
             print(x_prev)
-            if check(opt.g(x_prev), 10**(-4)):
+            
+            if check(opt.g(x_prev), 10**(-5)):
                 return x_prev
-            print(opt.G(x_prev))
-            x_next = x_prev - np.dot(np.linalg.inv(opt.G(x_prev)), opt.g(x_prev))
+            print(opt.Ginv)
+            opt.posDefCheck(x_prev)
+            opt.invG(x_prev)
+            sK = -np.dot(opt.Ginv, opt.g(x_prev))
+            alphaK = opt.f(x_prev +alpha*sK)
+            x_next = x_prev + alphaK * sK
             x_prev = x_next
+            
 
     def exact_line_search(self):
         pass
@@ -90,6 +109,11 @@ if __name__ == '__main__':
     n = len(x)
     opt = OptimizationProblem(f, n)
     opt(4)
-    print(opt.g(x), '\n',  opt.G(x))
+    #print(opt.g(x), '\n',  opt.G(x))
     bm = BaseMethods(opt)
-    print(bm(x, 'newton'))
+    print(bm(x, 0.7, 'newton'))
+
+    
+
+
+
