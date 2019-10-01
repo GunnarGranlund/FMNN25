f4eb03a66c9a3a29df53c7466a77d7c8e347e0bb
johannaengman <55276333+johannaengman@users.noreply.github.com>
2019-10-01 13:37:38 +0200

    Add files via upload

--- P2.py
+++ P2.py
@@ -2,6 +2,7 @@ import numpy as np
 import scipy.linalg as sp
 import matplotlib.pyplot as plt
 import scipy.optimize as op
+from chebyquad_problem import *
 
 
 # ===============================================================================
@@ -20,21 +21,9 @@ def check(list1, val):
     return True
 
 
-def compare(bigger_list, smaller_list):
-    print("Bigger list: ", bigger_list)
-    print("Smaller list: ", smaller_list)
-    if len(bigger_list) != len(smaller_list):
-        print("Not comppareable lists!")
-        return False
-    for i in range(len(bigger_list)):
-        if bigger_list[i] < smaller_list[i]:
-            return False
-    return True
-
-
 class OptimizationProblem(object):
-    def __init__(self, f, n):
-        self.f = f
+    def __init__(self, function, n):
+        self.f = function
         self.h = 0.00001
         self.n = n
         self.Ginv = np.eye(n)
@@ -47,7 +36,7 @@ class OptimizationProblem(object):
         for i in range(n):  # stepsize, h should be added depending on what partial derivative we want
             e = np.zeros(n)  # to caclulate
             e[i] = self.h
-            g[i] = (f(x + e) - f(x)) / self.h
+            g[i] = (self.f(x + e) - self.f(x)) / self.h
         return g
 
     def G(self, x):
@@ -58,9 +47,8 @@ class OptimizationProblem(object):
                 h2 = np.zeros(n)
                 h1[i] = self.h
                 h2[j] = self.h
-                G[i, j] = (f(x + h1 + h2) - f(x + h1) - f(x + h2) + f(x)) / self.h ** 2
+                G[i, j] = (self.f(x + h1 + h2) - self.f(x + h1) - self.f(x + h2) + self.f(x)) / self.h ** 2
                 if i != j:  # Symmetrizing step
-                    # print(G[i, j])
                     if G[i, j] == 0:
                         G[i, j] = 0.00001
                     G[i, j] = G[j, i]
@@ -80,8 +68,8 @@ class OptimizationProblem(object):
 
 
 class BaseMethods:
-    def __init__(self, opt):
-        self.opt = opt
+    def __init__(self, optimizer):
+        self.optimizer = optimizer
 
     def __call__(self, type, initial_guess=None):
         if type == 'exact':
@@ -91,27 +79,12 @@ class BaseMethods:
         else:
             print("Can't find given type.")
 
-    def newton_exact(self, x_prev):
-        x_px = np.array(())
-        x_py = np.array(())
-        while 1:
-            if check(opt.g(x_prev), 0.05):  # Check is in wrong way????
-                return x_prev, x_px, x_py
-            opt.posDefCheck(x_prev)
-            opt.invG(x_prev)
-            s_k = -np.dot(opt.Ginv, opt.g(x_prev))
-            alpha_k = op.fmin(self.f_alpha, 1, (x_prev, s_k), disp=0)
-            x_next = x_prev + alpha_k * s_k
-            x_px = np.append(x_px, x_prev[0])
-            x_py = np.append(x_py, x_prev[1])
-            x_prev = x_next
-
     def f_alpha(self, x_prev, alpha, s_k):
-        return f(x_prev + alpha * s_k)
-    
-    def f_prim_alpha(self, x, alpha, s_k):  
-        e = opt.h
-        return (self.f_alpha(x, alpha + e, s_k) - self.f_alpha(x, alpha, s_k))/opt.h 
+        return self.optimizer.f(x_prev + alpha * s_k)
+
+    def f_prim_alpha(self, x, alpha, s_k):
+        e = self.optimizer.h
+        return (self.f_alpha(x, alpha + e, s_k) - self.f_alpha(x, alpha, s_k)) / self.optimizer.h
 
     def extrapolation(self, alpha_zero, alpha_lower, x, s_k):
         return (alpha_zero - alpha_lower) * (self.f_prim_alpha(x, alpha_zero, s_k) /
@@ -130,6 +103,21 @@ class BaseMethods:
         rho = 0.1
         return self.f_alpha(x_prev, alpha_lower, s_k) + rho * (alpha_zero - alpha_lower) * \
                self.f_prim_alpha(x_prev, alpha_lower, s_k) >= self.f_alpha(x_prev, alpha_zero, s_k)
+
+    def newton_exact(self, x_prev):
+        x_px = np.array(())
+        x_py = np.array(())
+        while 1:
+            if check(self.optimizer.g(x_prev), 0.05):  # Check is in wrong way????
+                return x_prev, x_px, x_py
+            self.optimizer.posDefCheck(x_prev)
+            self.optimizer.invG(x_prev)
+            s_k = -np.dot(self.optimizer.Ginv, self.optimizer.g(x_prev))
+            alpha_k = op.fmin(self.f_alpha, 1, (x_prev, s_k), disp=0)
+            x_next = x_prev + alpha_k * s_k
+            x_px = np.append(x_px, x_prev[0])
+            x_py = np.append(x_py, x_prev[1])
+            x_prev = x_next
 
     def inexact_line_search(self, alpha_zero, alpha_lower, alpha_upper, x_prev, s_k):
         tau = 0.1
@@ -157,11 +145,11 @@ class BaseMethods:
         x_px = np.array(())
         x_py = np.array(())
         while 1:
-            if check(opt.g(x_prev), 0.05):
+            if check(self.optimizer.g(x_prev), 0.05):
                 return x_prev, x_px, x_py
-            opt.posDefCheck(x_prev)
-            opt.invG(x_prev)
-            s_k = -np.dot(opt.Ginv, opt.g(x_prev))
+            self.optimizer.posDefCheck(x_prev)
+            self.optimizer.invG(x_prev)
+            s_k = -np.dot(self.optimizer.Ginv, self.optimizer.g(x_prev))
             alpha_zero = self.inexact_line_search(alpha_zero, alpha_lower, alpha_upper, x_prev, s_k)
             x_next = x_prev + alpha_zero * s_k
             x_px = np.append(x_px, x_prev[0])
@@ -169,14 +157,14 @@ class BaseMethods:
             x_prev = x_next
 
 
-def f(x):
+def rosenbrock(x):
     return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
 
 
 def contour_plot(bm, type, x):
     minimum, minix, miniy = bm(type, x)
     X, Y = np.meshgrid(np.linspace(-0.5, 2, 1000), np.linspace(-0.5, 4, 1000))
-    Z = f([X, Y])
+    Z = rosenbrock([X, Y])
     """plt.figure(1)
     plt.contour(X, Y, Z, [0, 0.1, 0.5, 1, 2, 3, 5, 10, 15, 20, 50, 100, 200, 300, 400,
                           500, 600, 700, 800], colors='black')
@@ -191,13 +179,16 @@ def contour_plot(bm, type, x):
 
 
 if __name__ == '__main__':
-    x1 = 1.0
-    x2 = 1.1
+    x1 = 0.6
+    x2 = 0.8
     x = np.append(x1, x2)
     n = len(x)
-    opt = OptimizationProblem(f, n)
-    bm = BaseMethods(opt)
+    #opt = OptimizationProblem(rosenbrock, n)
+    #bm = BaseMethods(opt)
     #print(bm('newton', x)[0])
     #print(bm('inexact', x)[0])
     #contour_plot(bm, 'exact', x)
-    contour_plot(bm, 'inexact', x)
+    #contour_plot(bm, 'inexact', x)
+    opt2 = OptimizationProblem(chebyquad, n)
+    bm2 = BaseMethods(opt2)
+    contour_plot(bm2, 'exact', x)
