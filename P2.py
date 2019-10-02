5cfd6b1beded026b675633807c8a1f27a8e13324
GunnarGranlund <55276362+GunnarGranlund@users.noreply.github.com>
2019-10-02 09:44:33 +0200

    Add files via upload

--- P2.py
+++ P2.py
@@ -48,9 +48,10 @@ class OptimizationProblem(object):
                 h1[i] = self.h
                 h2[j] = self.h
                 G[i, j] = (self.f(x + h1 + h2) - self.f(x + h1) - self.f(x + h2) + self.f(x)) / self.h ** 2
-                if i != j:  # Symmetrizing step
+                if i == j:
                     if G[i, j] == 0:
                         G[i, j] = 0.00001
+                if i != j:      # Symmetrizing step
                     G[i, j] = G[j, i]
         return G
 
@@ -199,6 +200,7 @@ class BaseMethods:
             (np.dot(delta_k.T, gamma_k))
             H_prev = H_k
             x_prev = x_next
+            print(x_prev)
 
 def rosenbrock(x):
     return 100 * (x[1] - x[0] ** 2) ** 2 + (1 - x[0]) ** 2
@@ -214,23 +216,22 @@ def contour_plot(bm, type, x, function):
     plt.title('Rosenbrock function f(x,y) = 100(y-x^2)^2+(1-x)^2')"""
     plt.figure(2)
     plt.contour(X, Y, Z, [1, 3.831, 14.678, 56.234, 215.443, 825.404], colors='black')
-    print(minix, miniy)
-    print(minimum)
     plt.plot(minix, miniy, color='k', marker='o', ls='-.')
     plt.plot(minimum[0], minimum[1], color='r', marker='o', ls='-.')
     plt.show()
 
 
 if __name__ == '__main__':
-    x1 = 0.6
-    x2 = 0.5
+    x1 = 1.0
+    x2 = 1.1
     x = np.append(x1, x2)
     n = len(x)
     opt = OptimizationProblem(rosenbrock, n)
     bm = BaseMethods(opt)
-    print(bm('exact', x)[0])
+    #print(bm('exact', x)[0])
     #print(bm('inexact', x)[0])
-    contour_plot(bm, 'exact', x, rosenbrock)
+    print(bm('bfgs', x))
+    #contour_plot(bm, 'exact', x, rosenbrock)
     #contour_plot(bm, 'inexact', x, rosenbrock)
     #opt2 = OptimizationProblem(chebyquad, n)
     #bm2 = BaseMethods(opt2)
