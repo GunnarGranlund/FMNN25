ec1dd7b9d75bfedff99257f1728d660066b5c50f
johannaengman <55276333+johannaengman@users.noreply.github.com>
2019-10-10 15:01:48 +0200

    Add files via upload

--- P3.py
+++ P3.py
@@ -2,24 +2,9 @@ import numpy as np
 from scipy.linalg import toeplitz
 from scipy import linalg
 
-def second_order_diff(delta_x, u, i,  j):
-    u_ij = (u[i][j+1] + u[i][j-1] - 4*u[i][j], u[i+1][j] + u[i-1][j]) / (delta_x**2)
-    return u_ij
-
-if __name__ == '__main__':
-    room1 = np.zeros((10, 10))
-    room2 = np.zeros((10, 20))
-    room3 = np.zeros((10, 10))
-    omega = 0.8
-    delta_x = 1/20
-
-    normal_wall = 15
-    wall_heater = 40
-    wall_window = 5
 
 class RoomHeatingProblem:
-
-    def __init__(self, dx, type = None):
+    def __init__(self, dx, type=None):
         self.temp1 = 40
         self.temp2 = 15
         self.temp3 = 5
@@ -28,22 +13,22 @@ class RoomHeatingProblem:
         if type == 'first':
             X = 1
             Y = 1
-            self.grid1 = np.zeros((round(X/(self.dx)) + 1, round(Y/(self.dx)) + 1))
-            self.grid1[0] = self.grid1[-1] = self.grid1[:,0] = np.ones(len(self.grid1[0]))
+            self.grid1 = np.zeros((round(X / self.dx) + 1, round(Y / self.dx) + 1))
+            self.grid1[0] = self.grid1[-1] = self.grid1[:, 0] = np.ones(len(self.grid1[0]))
             self.grid1[0][0] = self.grid1[-1][0] = 1
-    
+
     def A_matrix(self):
         if self.type == 'first':
-            a = np.zeros(len(self.grid1[0])**2)
+            a = np.zeros(len(self.grid1[0]) ** 2)
             a[0] = -4
-            a[1] = a[len(a) - 1] = a[len(self.grid1[0])] = a[len(self.grid1[0]*(len(self.grid1[0] - 1)))] = 1
+            a[1] = a[len(a) - 1] = a[len(self.grid1[0])] = a[len(self.grid1[0] * (len(self.grid1[0] - 1)))] = 1
             A1 = toeplitz(a)
-            dX = (1/(self.dx**2))*np.eye(len(A1[0]))
+            dX = (1 / (self.dx ** 2)) * np.eye(len(A1[0]))
             k = 0
             bc = np.array([])
             for i in range(len(self.grid1[0])):
                 for j in range(len(self.grid1[0])):
-                    if(self.grid1[i][j]) == 0:
+                    if (self.grid1[i][j]) == 0:
                         bc = np.append(bc, k)
                     k += 1
         for i in range(len(a)):
@@ -51,6 +36,7 @@ class RoomHeatingProblem:
                 A1[i] = A1[i] - A1[i]
                 A1[i][i] = 1
         return A1
+
 
 class Solver:
     def __init__(self, heating_problem):
@@ -71,9 +57,9 @@ class Solver:
         return u1, u2
 
     def relaxation(self, old_u1, old_u2, old_u3, u1, u2, u3):
-        u1 = omega*u1 + (1-omega)*old_u1
-        u2 = omega*u2 + (1-omega)*old_u2
-        u3 = omega*u3 + (1-omega)*old_u3
+        u1 = omega * u1 + (1 - omega) * old_u1
+        u2 = omega * u2 + (1 - omega) * old_u2
+        u3 = omega * u3 + (1 - omega) * old_u3
         return u1, u2, u3
 
     def iterate(self):
@@ -96,13 +82,11 @@ if __name__ == '__main__':
     room2 = np.zeros((10, 20))
     room3 = np.zeros((10, 10))
     omega = 0.8
-    delta_x = 1/20
+    delta_x = 1 / 20
 
     normal_wal = 15
     wall_heater = 40
     wall_window = 5
 
-
-    Room1 = RoomHeatingProblem(1/3, type = 'first')
+    Room1 = RoomHeatingProblem(1 / 3, type='first')
     print(Room1.A_matrix())
-
