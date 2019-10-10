78865db3937ce3b111455c11afe8f67a82e89043
johannaengman <55276333+johannaengman@users.noreply.github.com>
2019-10-10 10:36:44 +0200

    Add files via upload

---
+++ P3.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+if __name__ == '__main__':
+    room1 = np.zeros((10, 10))
+    room2 = np.zeros((10, 20))
+    room3 = np.zeros((10, 10))
+    omega = 0.8
+    delta_x = 1/20
+
