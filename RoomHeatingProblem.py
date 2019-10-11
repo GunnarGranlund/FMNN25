9a23c755e20c7889ee09c4c9473a6e373b234ab6
GunnarGranlund <55276362+GunnarGranlund@users.noreply.github.com>
2019-10-11 10:32:05 +0200

    Add files via upload

---
+++ RoomHeatingProblem.py
@@ -0,0 +1,132 @@
+import numpy as np
+from scipy.linalg import toeplitz
+from scipy import linalg
+import matplotlib.pyplot as plt
+
+
+class RoomHeatingProblem:
+    def __init__(self, dx, type=None):
+        self.heating_wall = 40
+        self.normal_wall = 15
+        self.window_wall = 5
+        self.dx = dx
+        self.type = type
+        if type == 'first':
+            X = 1
+            Y = 1
+            self.grid1 = np.zeros((round(X / self.dx) + 1, round(Y / self.dx) + 1))
+            self.grid1[0] = self.grid1[-1] = self.grid1[:, 0] = np.ones(len(self.grid1[0]))
+            self.grid1[0][0] = self.grid1[-1][0] = 1
+            self.grid1[0:len(self.grid1[0]), len(self.grid1[0]) - 1 ] = 2
+            self.u1_r1 = np.array([])
+        if type == 'second':
+            X = 2
+            Y = 1
+            self.grid2 = np.zeros((round(X / self.dx) + 1, round(Y / self.dx) + 1))
+            self.grid2[0] = self.grid2[-1] = np.ones(len(self.grid2[0]))
+            self.grid2[0:len(self.grid2[0]), 0] = self.grid2[len(self.grid2[0]) \
+                         - 1:2 * len(self.grid2[0]), len(self.grid2[0]) - 1] = 1
+            self.grid2[-1][-1] = self.grid2[0][-1] = 1
+            self.grid2[len(self.grid2[0])-1:len(self.grid2[:,0]), 0] = 2
+            self.grid2[0:len(self.grid2[0]), len(self.grid2[0])-1] = 3
+            self.u2_r1 = np.array([])
+            self.u2_r2 = np.array([])
+        if type == 'third':
+            X = 1
+            Y = 1
+            self.grid3 = np.zeros((round(X / self.dx) + 1, round(Y / self.dx) + 1))
+            self.grid3[0] = self.grid3[-1] = self.grid3[:, len(self.grid3[0]) - 1] = np.ones(len(self.grid3[0]))
+            self.grid3[-1][-1] = self.grid3[0][-1] = 1
+            self.grid3[0:len(self.grid3[0]), 0] = 2
+            self.u3_r2 = np.array([])
+        A, b = self.A_matrix()
+        self.A = A
+        self.b = b 
+    def __call__(self):
+        A, b = self.A_matrix()
+        if self.type == 'first':
+            grid = self.grid1
+        elif self.type == 'second':
+            grid = self.grid2
+        elif self.type == 'third':
+            grid = self.grid3
+        return A, b, grid
+
+    def A_matrix(self):
+        if self.type == 'first':
+            a = np.zeros(len(self.grid1[0]) ** 2)
+            a[0] = -4
+            a[1] = a[len(a) - 1] = a[len(self.grid1[0])] = a[len(self.grid1[0] * (len(self.grid1[0] - 1)))] = 1
+            A = toeplitz(a)
+            dX = (1 / (self.dx ** 2)) * np.eye(len(A[0]))
+            k = 0
+            bc = np.array([])
+            b = np.zeros(len(a))
+            for i in range(len(self.grid1[0])):
+                for j in range(len(self.grid1[0])):
+                    if (self.grid1[i][j]) == 1:
+                        bc = np.append(bc, k)
+                    elif self.grid1[i][j] == 2:
+                        self.u1_r1 = np.append(self.u1_r1, k)
+                    k += 1
+
+        if self.type == 'second':
+            a = np.zeros(len(self.grid2[0]) * len(self.grid2[:, 0]))
+            a[0] = -4
+            a[1] = a[len(a) - 1] = a[len(self.grid2[0])] = a[len(self.grid2[0] * (len(self.grid2[0] - 1)))] = 1
+            A = toeplitz(a)
+            dX = (1 / (self.dx ** 2)) * np.eye(len(A[0]))
+            k = 0
+            bc = np.array([])
+            b = np.zeros(len(a))
+            for i in range(len(self.grid2[:, 0])):
+                for j in range(len(self.grid2[0])):
+                    if (self.grid2[i][j]) == 1:
+                        bc = np.append(bc, k)
+                    elif self.grid2[i][j] == 2:
+                        self.u2_r1 = np.append(self.u2_r1, k)
+                    elif self.grid2[i][j] == 3:
+                        self.u2_r2 = np.append(self.u2_r2, k)
+                    k += 1
+
+        if self.type == 'third':
+            a = np.zeros(len(self.grid3[0]) ** 2)
+            a[0] = -4
+            a[1] = a[len(a) - 1] = a[len(self.grid3[0])] = a[len(self.grid3[0] * (len(self.grid3[0] - 1)))] = 1
+            A = toeplitz(a)
+            dX = (1 / (self.dx ** 2)) * np.eye(len(A[0]))
+            k = 0
+            bc = np.array([])
+            b = np.zeros(len(a))
+            for i in range(len(self.grid3[0])):
+                for j in range(len(self.grid3[0])):
+                    if (self.grid3[i][j]) == 1:
+                        bc = np.append(bc, k)
+                    if self.grid3[i][j] == 2:
+                        self.u3_r2 = np.append(self.u3_r2, k)
+                    k += 1
+        for i in range(len(a)):
+            if i in bc:
+                A[i] = A[i] - A[i]
+                A[i][i] = self.dx ** 2
+
+        if self.type == 'first':
+            b[1:len(self.grid1[0])] = b[1 + len(self.grid1[0]) * 3:len(b)] = self.normal_wall
+            for i in range(len(self.grid1[0, :])):
+                b[i * len(self.grid1[0])] = self.heating_wall
+
+        if self.type == 'second':
+            for i in range(len(self.grid2[0])):
+                b[i * len(self.grid2[0])] = self.normal_wall
+            for i in range(len(self.grid2[0])):
+                b[i * len(self.grid2[0]) + len(self.grid2[0]) ** 2 - 1] = self.normal_wall
+            b[0:len(self.grid2[0])] = self.heating_wall
+            b[len(b) - len(self.grid2[0]):len(b)] = self.window_wall
+
+        if self.type == 'third':
+            b[0:len(self.grid3[0])] = b[len(self.grid3[0]) * 3:len(b)] = self.normal_wall
+            for i in range(len(self.grid3[0])):
+                b[i * len(self.grid3[0]) + len(self.grid3[0]) - 1] = self.heating_wall
+        return np.dot(dX, A), b
+
+Room1 = RoomHeatingProblem(1/3, type='first')
